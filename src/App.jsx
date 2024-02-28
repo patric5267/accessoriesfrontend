f33f4ec399da17de6b4178accd1c0971a2e80c87
patric5267 <115068229+patric5267@users.noreply.github.com>
2024-02-28 23:10:15 +0530

    added lazyload

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,35 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home'
-import Productpage from './pages/Productpage'
-import Allproducts from './pages/Allproducts'
-import Cart from './pages/Cart'
-import Categoryproduct from './pages/Categoryproduct'
-import Checkout from './pages/Checkout'
+import { Suspense, lazy } from 'react'
+// import Home from './pages/Home'
+// import Productpage from './pages/Productpage'
+// import Allproducts from './pages/Allproducts'
+// import Cart from './pages/Cart'
+// import Checkout from './pages/Checkout'
+const Home = lazy(()=>import('./pages/Home'))
+const Productpage =lazy(()=>import('./pages/Productpage'))
+const Allproducts = lazy(()=>import('./pages/Allproducts'))
+const Cart = lazy(()=>import('./pages/Cart'))
+const Categoryproduct=lazy(()=>import('./pages/Categoryproduct'))
+const Checkout=lazy(()=>import('./pages/Checkout'))
 function App() {
  
   return (
     <Router >
       <Navbar />
       <Routes>
+        {/* <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<Productpage />}/>
+        <Route path='/allproducts' element={<Allproducts />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/category/:categoryitem' element={<Categoryproduct/>} />
+        <Route path='/checkout/:title/:id/:quantity' element={<Checkout/>} /> */}
         <Route path='/' element={<Home />} />
         <Route path='/product/:id' element={<Productpage />} />
         <Route path='/allproducts' element={<Allproducts />} />
         <Route path='/cart' element={<Cart />} />
-        <Route path='/category/:categoryitem' element={<Categoryproduct />} />
+        <Route path='/category/:categoryitem' element={<Categoryproduct/>} />
         <Route path='/checkout/:title/:id/:quantity' element={<Checkout />} />
       </Routes>
      <Footer/>
--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { images } from '../Extras/Brand';
-import { Autoplay} from 'swiper/modules';
+import { Autoplay ,EffectFade} from 'swiper/modules';
 import 'swiper/css';
-
+import 'swiper/css/effect-fade';
 const Herosection = () => {
     return (
         <div className='xl:flex xl:justify-center' id="home">
             <div className=' xl:w-[80rem] px-3 flex flex-col sm:flex-row sm:items-center lg:justify-between gap-4  lg:gap-10 sm:h-[100vh]'>
                 <div className="heroimg sm:order-2 sm:w-[50%]    ">
                     <Swiper 
-                    modules={[Autoplay]}
+                    modules={[Autoplay,EffectFade]}
                     loop={true}
                     centeredSlides={true}
+                    effect={'fade'}
                     autoplay={{
-                      delay: 2500,
+                      delay: 4000,
                       disableOnInteraction: false,
                     }}
                     slidesPerView={1} >
