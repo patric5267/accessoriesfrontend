8d197c98c8d67cfd5444a76938ee1600d2d6fd7a
patric5267 <115068229+patric5267@users.noreply.github.com>
2024-02-29 15:56:29 +0530

    changed fontfamily below 640px

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,18 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { Suspense, lazy } from 'react'
-// import Home from './pages/Home'
-// import Productpage from './pages/Productpage'
-// import Allproducts from './pages/Allproducts'
-// import Cart from './pages/Cart'
-// import Checkout from './pages/Checkout'
-const Home = lazy(()=>import('./pages/Home'))
-const Productpage =lazy(()=>import('./pages/Productpage'))
-const Allproducts = lazy(()=>import('./pages/Allproducts'))
-const Cart = lazy(()=>import('./pages/Cart'))
-const Categoryproduct=lazy(()=>import('./pages/Categoryproduct'))
-const Checkout=lazy(()=>import('./pages/Checkout'))
+import Home from './pages/Home'
+import Productpage from './pages/Productpage'
+import Allproducts from './pages/Allproducts'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Categoryproduct from './pages/Categoryproduct'
 function App() {
  
   return (
     <Router >
       <Navbar />
       <Routes>
-        {/* <Route path='/' element={<Home />} />
-        <Route path='/product/:id' element={<Productpage />}/>
-        <Route path='/allproducts' element={<Allproducts />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/category/:categoryitem' element={<Categoryproduct/>} />
-        <Route path='/checkout/:title/:id/:quantity' element={<Checkout/>} /> */}
         <Route path='/' element={<Home />} />
         <Route path='/product/:id' element={<Productpage />} />
         <Route path='/allproducts' element={<Allproducts />} />
